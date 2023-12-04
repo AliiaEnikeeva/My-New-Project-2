d2bbfe7888b0d928627722018ddaf5db611ce973
AliiaEnikeeva <116454309+AliiaEnikeeva@users.noreply.github.com>
2023-12-04 21:35:57 +0500

    Готовый калькулятор

--- Homework4.java
+++ Homework4.java
@@ -11,13 +11,28 @@ public class Homework4 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число: ");
         int number_a = Integer.parseInt(scanner.next());
-        System.out.println("Введите вид операции (): ");
+        System.out.println("Введите вид операции (+, -, *, /):  ");
         String operation = scanner.next();
         System.out.println("Введите второе число: ");
         int number_b = Integer.parseInt(scanner.next());
         int result;
         switch (operation) {
-           
+           case "+" -> {
+                result = number_a + number_b;
+                System.out.println("Сумма чисел равна:" + (result));
+            }
+            case "-" -> {
+                result = number_a - number_b;
+                System.out.println("Разность чисел равна:" + (result));
+            }
+            case "*" -> {
+                result = number_a * number_b;
+                System.out.println("Произведение чисел равно:" + (result));
+            }
+            case "/" -> {
+                result = number_a / number_b;
+                System.out.println("Результат деления чисел равен:" + (result));
+            }
         }
     }
 }
