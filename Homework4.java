523bc186af5964c83610ecd9b54ac0f4b8e21c65
Enikeeva Aliya <aliaa@mail.ru>
2023-12-04 23:09:30 +0500

    Удалили функцию умножения из калькулятора

--- Homework4.java
+++ Homework4.java
@@ -25,10 +25,7 @@ public class Homework4 {
                 result = number_a - number_b;
                 System.out.println("Разность чисел равна:" + (result));
             }
-            case "*" -> {
-                result = number_a * number_b;
-                System.out.println("Произведение чисел равно:" + (result));
-            }
+            
         }
     }
 }
