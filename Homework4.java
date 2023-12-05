b2dbe559ba32fb7738ba34ce2143ac4564a41e6a
Enikeeva Aliya <aliaa@mail.ru>
2023-12-05 22:44:08 +0500

    Revert "Удалили функцию деления из калькулятора" выполняя задание для homework02
    This reverts commit 3b9d984dd7028a493c46e30c53a3f7cc8d383bac.

--- Homework4.java
+++ Homework4.java
@@ -29,6 +29,10 @@ public class Homework4 {
                 result = number_a * number_b;
                 System.out.println("Произведение чисел равно:" + (result));
             }
+            case "/" -> {
+                result = number_a / number_b;
+                System.out.println("Результат деления чисел равен:" + (result));
+            }
         }
     }
 }
