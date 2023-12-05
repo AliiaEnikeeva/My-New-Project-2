0fd9a652adec7d028c00fb45f8a265803f3ef76f
Enikeeva Aliya <aliaa@mail.ru>
2023-12-05 23:32:20 +0500

    удаление вывода результата вычитания

--- Homework4.java
+++ Homework4.java
@@ -23,7 +23,7 @@ public class Homework4 {
             }
             case "-" -> {
                 result = number_a - number_b;
-                System.out.println("Разность чисел равна:" + (result));
+                
             }
             case "*" -> {
                 result = number_a * number_b;
@@ -31,7 +31,7 @@ public class Homework4 {
             }
             case "/" -> {
                 result = number_a / number_b;
-                
+            
             }
         }
     }
